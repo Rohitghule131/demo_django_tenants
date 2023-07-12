46e5b7acbc8e031c6b428929bef64a41d51af461
Rohit Ghule <rohit.ghule@mindbowser.com>
2023-07-12 15:11:17 +0530

    added models

--- myapp/models.py
+++ myapp/models.py
@@ -6,3 +6,11 @@ class DummyModel(models.Model):
     Class for create Dummy model.
     """
     name = models.CharField(max_length=100)
+
+
+class DummyModel2(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class DummyModel3(models.Model):
+    name = models.CharField(max_length=100)
--- tenant_script.py
+++ tenant_script.py
@@ -1,21 +1,21 @@
 from tenant_model.models import ClientModel, DomainModel
 
 # create your public tenant
-tenant = ClientModel(schema_name='public',
-                     name='Schemas Inc.',
-                     paid_until='2016-12-05',
-                     on_trial=False)
-tenant.save()
-
-# Add one or more domains for the tenant
-domain = DomainModel()
-domain.domain = 'localhost'  # don't add your port or www here! on a local server you'll want to use localhost here
-domain.tenant = tenant
-domain.is_primary = True
-domain.save()
+# tenant = ClientModel(schema_name='public',
+#                      name='Schemas Inc.',
+#                      paid_until='2016-12-05',
+#                      on_trial=False)
+# tenant.save()
+#
+# # Add one or more domains for the tenant
+# domain = DomainModel()
+# domain.domain = 'localhost'  # don't add your port or www here! on a local server you'll want to use localhost here
+# domain.tenant = tenant
+# domain.is_primary = True
+# domain.save()
 
 # create your first real tenant
-tenant = ClientModel(schema_name='tenant1',
+tenant = ClientModel(schema_name='tenant2',
                      name='Megatron',
                      paid_until='2014-12-05',
                      on_trial=True)
